85ceee5d3a9cdc49cb64f875c90076eb19ab5498
choisuyong44 <tlgmdtl1118@inu.ac.kr>
2024-11-08 17:14:18 +0900

    feat/#1,2-loginwithjwt

--- src/main/java/com/lswr/demo/controller/UserController.java
+++ src/main/java/com/lswr/demo/controller/UserController.java
@@ -2,18 +2,23 @@ package com.lswr.demo.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.RequestContext;
 
 import com.lswr.demo.model.dto.EmailDto;
 import com.lswr.demo.model.dto.LoginDto;
 import com.lswr.demo.model.dto.User;
 import com.lswr.demo.model.service.MailSenderService;
+import com.lswr.demo.model.service.TokenService;
 import com.lswr.demo.model.service.UserService;
 
 import jakarta.mail.MessagingException;
@@ -28,9 +33,11 @@ public class UserController {
 
 	private final UserService userService;
 	private final MailSenderService mailService;
+	private final TokenService tokenService;
 	
 	@GetMapping("/")
-	public ResponseEntity<?> getUserList() {
+	public ResponseEntity<?> getUserList(@RequestAttribute("userId") String userId) {
+		log.info(userId);
 		List<User> list = userService.getUserList();
 		return ResponseEntity.ok(list);
 	}
@@ -59,8 +66,14 @@ public class UserController {
 	
 	@PostMapping("/login")
 	public ResponseEntity<String> loginUser(@RequestBody LoginDto loginDto){
+		// 로그인 성공
 		if(userService.loginUser(loginDto)) {
-			return new ResponseEntity<String>("GOOD JOB",HttpStatus.OK);
+			User user = userService.getUser(loginDto.getEmail());
+            String token = tokenService.createToken(String.valueOf(user.getUserId())); // JWT 생성
+            log.info("userId:"+String.valueOf(user.getUserId()));
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Bearer " + token); // Authorization 헤더에 JWT 토큰 추가
+            return new ResponseEntity<>("GOOD JOB", headers, HttpStatus.OK);
 		}
 		return new ResponseEntity<String>("Not User",HttpStatus.BAD_REQUEST);
 	}
