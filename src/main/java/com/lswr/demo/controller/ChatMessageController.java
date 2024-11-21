9e98a5eecc8e504505a7a3ce64e03575e755a555
choisuyong44 <tlgmdtl1118@inu.ac.kr>
2024-11-21 17:45:45 +0900

    feat/#7-chatting-UserImg

--- src/main/java/com/lswr/demo/controller/ChatMessageController.java
+++ src/main/java/com/lswr/demo/controller/ChatMessageController.java
@@ -8,7 +8,9 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import com.lswr.demo.model.dto.ChatMessage;
 import com.lswr.demo.model.dto.Party;
+import com.lswr.demo.model.dto.User;
 import com.lswr.demo.model.service.ChatRoomService;
+import com.lswr.demo.model.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,12 +20,16 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class ChatMessageController {
 
+	private final UserService userService;
 	private final ChatRoomService chatRoomService;
 	private final SimpMessageSendingOperations messagingTemplate;
 
 	@MessageMapping("/chat/message") // 클라이언트가 "/pub/chat/message"로 발행
 	public void sendMessage(@Payload ChatMessage message) {
 		message.setSendedDate(LocalDateTime.now());
+		User user = userService.getUserById(message.getUserId());
+		message.setNickname(user.getNickname());
+		message.setUserImg(user.getUserImg());
 		
 		if (message.getMessageType().equals("ENTER")) {
 			Party party = new Party();
